7a824861b1641e60252f17186c1f19c21c3af7ee
Kevin Mendez <kevinrmendez@gmail.com>
2019-02-05 19:52:53 +0200

    account manager tab added to magento panel

---
+++ Controller/Adminhtml/Get/Index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Vaimo\TestModule\Controller\Adminhtml\Get;
+
+
+class Index extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory = false;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    )
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend((__('Account Manager Grid')));
+
+        return $resultPage;
+    }
+
+}
